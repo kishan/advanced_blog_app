682161ee230acc3c8bb040c4b428d4962ed839da
Kishan Patel <kspatel2018@gmail.com>
2016-06-29 20:09:20 -0500

    add tagging system
    - use django-taggit
    - ability to attach tags to posts
    - list similar posts by tags

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -32,6 +33,8 @@ class Post(models.Model):
 
     objects = models.Manager() # The default manager.
     published = PublishedManager() # our custom manager.
+
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
-# from django.db.models import Count
+from django.db.models import Count
 
-# from taggit.models import Tag
+from taggit.models import Tag
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
 
-# not in use
-def post_list(request, category=None):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        # posts and tags have many to many relationship
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -22,9 +28,12 @@ def post_list(request, category=None):
     except EmptyPage:
         # If page is out of range deliver last page of results
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    return render(request, 'blog/post/list.html', {'page': page,
+                                                   'posts': posts,
+                                                   'tag': tag})
 
 
+# not in use
 # selected page passed in to template as page_obj
 # class-based view with pagination
 class PostListView(ListView):
@@ -57,9 +66,16 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
 
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags',
+                                                                             '-publish')[:4]
+
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
-                                                     'comment_form': comment_form})
+                                                     'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
